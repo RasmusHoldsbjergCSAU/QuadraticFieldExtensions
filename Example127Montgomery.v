deba0809144f6330cf70dab06bc3d6a186947c7c
RasmusHoldsbjerg <rahola@cs.au.dk>
2020-10-12 11:00:39 +0200

    Minor cosmetic changes

--- Example127Montgomery.v
+++ Example127Montgomery.v
@@ -31,6 +31,9 @@ Import Associational Positional.
 Local Open Scope Z_scope.
 Local Open Scope list_scope.
 
+(* Reifiable implementations of addition and multiplication in Fp2 with p = 2^127 - 1.
+   We represent elements af Fp2 as integers in montgomery form; proofs of correctness are included.*)
+
 (*Definitions and notation*)
 
 Definition s := (2 ^ 127)%Z.
@@ -51,7 +54,7 @@ Proof. apply Z.eqb_eq; auto. Qed.
 
 Lemma p_prime: (prime p).
 Proof. Admitted.
- (* assert (p = Mp 127) as Hmp by reflexivity; rewrite Hmp; 
+   (*assert (p = Mp 127) as Hmp by reflexivity. rewrite Hmp. 
     exact (LucasTest 127 (refl_equal _)). 
 Qed. *)
 
@@ -60,9 +63,9 @@ Notation "x *Fp y" := (GZnZ.mul p x y) (at level 90).
 Notation "x -Fp y" := (GZnZ.sub p x y) (at level 100).
 
 (*Base field arithmetic, encoding and decoding*)
-Definition mulp x y := (WordByWordMontgomery.mulmod 64 2 p 1 x y).
-Definition addp x y := (WordByWordMontgomery.addmod 64 2 p x y).
-Definition subp x y := (WordByWordMontgomery.submod 64 2 p x y).
+Definition mulp x y := WordByWordMontgomery.mulmod 64 2 p 1 x y.
+Definition addp x y := WordByWordMontgomery.addmod 64 2 p x y.
+Definition subp x y := WordByWordMontgomery.submod 64 2 p x y.
 Definition tomont z := WordByWordMontgomery.encodemod 64 2 p 1 z.
 Definition evalp z := @WordByWordMontgomery.eval 64 2 (WordByWordMontgomery.from_montgomerymod 64 2 p 1 z).
 
@@ -94,54 +97,57 @@ Proof. intros. assert (H: (-1 zmod p) = GZnZ.opp p (one p)) by auto; rewrite H; 
 
 Lemma zpz_le_p: forall x, 0 <= val p x < p.
 Proof.
-    intros x; case x; intros val Hval; simpl; rewrite Hval; apply Z_mod_lt; unfold p; auto with zarith.
+    intros [val Hval]; simpl; rewrite Hval; apply Z_mod_lt; unfold p; auto with zarith.
 Qed.
 
 Lemma karatsuba_correct: forall x1 x2 y1 y2, ((x1 +Fp x2) *Fp (y1 +Fp y2) -Fp x1 *Fp y1 -Fp x2 *Fp y2) = (x1 *Fp y2 +Fp x2 *Fp y1).
 Proof. intros; ring. Qed.
 
+Ltac lauto := auto with zarith; unfold p; lia.
 
 (*Proofs of validity (as in Crypto.Arithmetic.WordByWordMontgomery)*)
+
+
+(*Se på goal selection!!!*)
 Lemma tomont_valid: forall x, WordByWordMontgomery.valid 64 2 p (tomont (val p x)).
 Proof.
-    intros; unfold tomont; pose proof WordByWordMontgomery.encodemod_correct 64 2 p (2^63) 1. destruct H as [_ H0];
-    [auto| auto| lia| unfold p; lia| auto| unfold p; lia|]; apply H0; case x; intros; auto; simpl; rewrite inZnZ; apply Z_mod_lt; unfold p; lia.
+    intros; unfold tomont; pose proof WordByWordMontgomery.encodemod_correct 64 2 p (2^63) 1 as [_ H0].
+    1-6: lauto. apply H0; case x; intros; auto; simpl; rewrite inZnZ; apply Z_mod_lt; unfold p; lia.
 Qed.
 
 Lemma add_valid: forall x y,WordByWordMontgomery.valid 64 2 p x -> WordByWordMontgomery.valid 64 2 p y -> WordByWordMontgomery.valid 64 2 p (x +p y).
 Proof.
-    intros x y Hx Hy; pose proof WordByWordMontgomery.addmod_correct 64 2 p (2^63) 1; destruct H as [_ H0];
-    [auto| auto| lia| unfold p; lia| auto| unfold p; lia|]; apply H0; auto.
+    intros; pose proof WordByWordMontgomery.addmod_correct 64 2 p (2^63) 1 as [_ H1].
+    1-6: lauto. apply H1; auto.
 Qed.
 
 Lemma sub_valid: forall x y,WordByWordMontgomery.valid 64 2 p x -> WordByWordMontgomery.valid 64 2 p y -> WordByWordMontgomery.valid 64 2 p (x -p y).
 Proof.
-    intros x y Hx Hy; pose proof WordByWordMontgomery.submod_correct 64 2 p (2^63) 1; destruct H as [_ H0];
-    [auto| auto| lia| unfold p; lia| auto| unfold p; lia|]; apply H0; auto.
+    intros; pose proof WordByWordMontgomery.submod_correct 64 2 p (2^63) 1 as [_ H1].
+    1-6: lauto. apply H1; auto.
 Qed.
 
 Lemma mul_valid: forall x y,WordByWordMontgomery.valid 64 2 p x -> WordByWordMontgomery.valid 64 2 p y -> WordByWordMontgomery.valid 64 2 p (x *p y).
 Proof.
-    intros x y Hx Hy. pose proof WordByWordMontgomery.mulmod_correct 64 2 p (2^63) 1. destruct H;
-    [auto| auto| lia| unfold p; lia| auto| unfold p; lia|]. apply H0; auto.
+    intros; pose proof WordByWordMontgomery.mulmod_correct 64 2 p (2^63) 1 as [_ H1].
+    1-6: lauto. apply H1; auto.
 Qed.
 
 
 (*Correctness of evaluation wrt. operations and encoding*)
 Theorem eval_tomont_inv: forall val, 0 <= val < p -> evalp (tomont val) mod p = val.
 Proof.
-    intros val H; unfold evalp, tomont; rewrite WordByWordMontgomery.eval_encodemod with (r' := 2 ^ 63);
-    (unfold p, s, c; simpl; unfold Z.pow_pos; simpl; auto with zarith; lia).
+    intros val H; unfold evalp, tomont; rewrite WordByWordMontgomery.eval_encodemod with (r' := 2 ^ 63); try lauto.
 Qed.
 
 Lemma eval_sub: forall x y, WordByWordMontgomery.valid 64 2 p x -> WordByWordMontgomery.valid 64 2 p y -> (evalp (x -p y)) mod p = ( evalp x - evalp y ) mod p.
-Proof. intros; unfold evalp; rewrite WordByWordMontgomery.eval_submod with (r' := 2 ^ 63); auto; try (unfold p; lia). Qed.
+Proof. intros; unfold evalp; rewrite WordByWordMontgomery.eval_submod with (r' := 2 ^ 63); lauto. Qed.
 
 Lemma eval_add: forall x y, WordByWordMontgomery.valid 64 2 p x -> WordByWordMontgomery.valid 64 2 p y -> (evalp (x +p y)) mod p = ( evalp x + evalp y ) mod p.
-Proof. intros; unfold evalp; rewrite WordByWordMontgomery.eval_addmod with (r' := 2 ^ 63); auto; try (unfold p; lia). Qed.
+Proof. intros; unfold evalp; rewrite WordByWordMontgomery.eval_addmod with (r' := 2 ^ 63); lauto. Qed.
 
 Lemma eval_mul: forall x y, WordByWordMontgomery.valid 64 2 p x -> WordByWordMontgomery.valid 64 2 p y -> (evalp (x *p y)) mod p = ( evalp x * evalp y ) mod p.
-Proof. intros; unfold evalp; rewrite WordByWordMontgomery.eval_mulmod with (r' := 2 ^ 63); auto; try (unfold p; lia). Qed.
+Proof. intros; unfold evalp; rewrite WordByWordMontgomery.eval_mulmod with (r' := 2 ^ 63); lauto. Qed.
 
 Lemma eval_sub_mod: forall x y, WordByWordMontgomery.valid 64 2 p x -> WordByWordMontgomery.valid 64 2 p y -> (evalp (x -p y)) mod p = ( evalp x mod p - evalp y mod p ) mod p.
 Proof. intros; rewrite eval_sub; try assumption; apply Zminus_mod. Qed.
@@ -157,29 +163,29 @@ Proof. intros; rewrite eval_mul; try assumption; apply Z.mul_mod; unfold p; auto
 Theorem addFp2_correct: forall x y, evalFp2 (addFp2 (tomontFp2 x) (tomontFp2 y)) = addp2 p x y.
 Proof.
     intros; apply Fp2irr; simpl; apply zirr; rewrite eval_add_mod; try apply tomont_valid;
-    repeat rewrite eval_tomont_inv; auto; apply zpz_le_p.
+    rewrite 2!eval_tomont_inv; auto; apply zpz_le_p.
 Qed.
 
 Theorem subFp2_correct: forall x y , evalFp2 (subFp2 (tomontFp2 x) (tomontFp2 y)) = subp2 p x y.
 Proof.
     intros; apply Fp2irr; simpl; apply zirr; rewrite eval_sub_mod; try apply tomont_valid;
-    repeat rewrite eval_tomont_inv; auto; apply zpz_le_p.
+    rewrite 2!eval_tomont_inv; auto; apply zpz_le_p.
 Qed.
 
 Theorem mul_correct: forall x y, evalFp2 (mulFp2 (tomontFp2 x) (tomontFp2 y)) = mulp2 p x y.
 Proof.
     intros x y. unfold mulFp2, mulp2. rewrite quad_res_minus_1. simpl. apply Fp2irr.
     - simpl; rewrite Fp2_opp_sub_equiv; apply zirr; rewrite eval_sub_mod; try (apply mul_valid; apply tomont_valid);
-      repeat rewrite eval_mul_mod; try apply tomont_valid; repeat rewrite eval_tomont_inv; try apply zpz_le_p; reflexivity.
-    - rewrite <- karatsuba_correct; simpl; apply zirr; repeat rewrite eval_sub_mod;
+      rewrite 2!eval_mul_mod; try apply tomont_valid; rewrite 4!eval_tomont_inv; try apply zpz_le_p; reflexivity.
+    - rewrite <- karatsuba_correct; simpl; apply zirr; rewrite 2!eval_sub_mod;
       [| apply mul_valid; apply add_valid; apply tomont_valid
        | apply mul_valid; apply tomont_valid
        | apply sub_valid; apply mul_valid; [apply add_valid| apply add_valid| |]; apply tomont_valid
        | apply mul_valid; apply tomont_valid].
       rewrite eval_mul_mod; try ( try apply add_valid; apply tomont_valid);
-      repeat rewrite eval_mul_mod; try apply tomont_valid;
+      rewrite 2!eval_mul_mod; try apply tomont_valid;
       rewrite eval_add_mod; try apply tomont_valid;
-      rewrite eval_add_mod; try apply tomont_valid; repeat rewrite eval_tomont_inv; try apply zpz_le_p; reflexivity.
+      rewrite eval_add_mod; try apply tomont_valid; rewrite 4!eval_tomont_inv; try apply zpz_le_p; reflexivity.
 Qed.
 
 Theorem to_montFp2_correct: forall x y, evalFp2 (tomontFp2 (x, y)) = (x, y).
@@ -205,17 +211,17 @@ Definition  mulFp2r x1 x2 y1 y2 :=
             ).
 
 Lemma mulFp2r_correct: forall x y, mulFp2r (fst x) (snd x) (fst y) (snd y) = mulFp2 x y.
-Proof. intros x y; destruct x, y. auto. Qed.
+Proof. intros [] []; auto. Qed.
 
 Definition addFp2r x1 x2 y1 y2 := (x1 +p y1, x2 +p y2).
 
 Lemma addFp2r_correct: forall x y, addFp2r (fst x) (snd x) (fst y) (snd y) = addFp2 x y.
-Proof. intros x y; destruct x, y; auto. Qed.
+Proof. auto. Qed.
 
 Definition subFp2r x1 x2 y1 y2 := (x1 -p y1, x2 -p y2).
 
 Lemma subFp2r_correct: forall x y, subFp2r (fst x) (snd x) (fst y) (snd y) = subFp2 x y.
-Proof. intros x y; destruct x, y; auto. Qed.
+Proof. auto. Qed.
 
 (*Initializing parameters for reification*)
 Local Instance : split_mul_to_opt := None.
